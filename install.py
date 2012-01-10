597c351dd0f66f538bcd04d67be3c139da7c1f29
Kevin Reedy <kevinreedy@gmail.com>
2012-01-09 22:32:09 -0600

    Basics working,but has bugs

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os 
+import sys
 
 # Special files not to install into previous directory
 # TODO Make this work entire whole path so we can exclude .ssh/authorized_keys but not .something/authorized_keys 
@@ -11,14 +12,21 @@ special_files = {
 }
 
 def process_dir(dir_path):
-    # TODO Create new folder in parent if it doesn't exist (../.ssh/ for example) 
-    files = os.listdir(dir_path)
+    # Create the directory if it doesn't exist
+    if not os.path.exists("." + dir_path):
+        print "." + dir_path + " does not exist. Creating it."
+        os.mkdir("." + dir_path)
 
+
+    files = os.listdir(dir_path)
     for file_name in files:
         full_path = dir_path + "/" + file_name
+
+        # do nothing for certain files
         if file_name not in special_files:
+            # if this is a directory, call this function on it
             if os.path.isdir(full_path):
-                print "found a directory: %s" % (full_path,)
+                print "found directory: %s" % (full_path,)
                 process_dir(full_path)
             elif os.path.isfile(full_path):
                 process_file(full_path)
@@ -28,8 +36,15 @@ def process_dir(dir_path):
 def process_file(file_name):
     if file_name not in special_files:
         if os.path.isfile(file_name):
-            print "linking file: %s" %(file_name,)
-            # TODO actually link the file in .. (be sure to make sure .ssh/file works properly)
+            # if file already exists, make a backup
+            if os.path.exists("." + file_name):
+                print "." + file_name + " already exists. renaming it to " + file_name + ".backup"
+                os.rename("." + file_name, "." + file_name + ".backup")
+
+            # create a simlink
+            #TODO This is resulting in dotfiles/./.gitconfig, which doesn't work for subdirs and is ugly
+            print "creating symlink for " + file_name + " to ." + file_name 
+            os.symlink("dotfiles/" + file_name, "." + file_name)
         else:
             raise Exception("%s is not a file" %(file_name,))
 
