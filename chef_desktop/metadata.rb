4e3ebb3b019f1c727d0461aae7b6ec56d2ebdf62
Kevin Reedy <kevinreedy@gmail.com>
2015-11-19 22:55:33 -0600

    chef generate cookbook

---
+++ chef_desktop/metadata.rb
@@ -0,0 +1,7 @@
+name 'chef_desktop'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures chef_desktop'
+long_description 'Installs/Configures chef_desktop'
+version '0.1.0'
